916d6a80076d90b14da6e77a20309599940075a5
Avisek Das <avisekdas555@gmail.com>
2026-07-30 16:45:25 +0000

    branding: module author avisek, drop installer credit line

--- module/customize.sh
+++ module/customize.sh
@@ -183,5 +183,4 @@ rm -rf "${MODPATH:?}/bin" "$MODPATH/stock/"
 cp -f "$MODPATH/module.prop" "$MODPATH/module.prop.orig"
 
 ui_print "* Done. No need to reboot."
-ui_print "  by j-hc (github.com/j-hc)"
 ui_print " "
